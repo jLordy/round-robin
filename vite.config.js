94f1a78388771c94aae170dd329963f46a4eff9f
jLordy <jhaylordceleste1020@gmail.com>
2024-10-02 21:44:35 +0800

    Add files via upload

--- vite.config.js
+++ vite.config.js
@@ -3,12 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  build: {
-    rollupOptions: {
-      external: ['styled-components']
-    }
-  },
   plugins: [react()],
-  
 })
 
